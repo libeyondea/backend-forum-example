3313ffde6f6a628ba495839c095f32f6396621a5
nhtanonymous <57558120+de4th-zone@users.noreply.github.com>
2021-06-17 23:54:28 +0700

    Update AuthController.php

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -125,6 +125,7 @@ class AuthController extends ApiController
                 $user->user_name = 'fb_' . $profile['id'];
                 $user->password = bcrypt(Str::random(9));
                 $user->avatar = $avatarName;
+                $user->gender = 'unknown';
                 $user->role_id = Role::where('slug', 'user')->first()->id;
 
                 if (!User::where('email', $profile['email'])->first()) {
